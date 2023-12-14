b9a90e7fe5e38eea5d7a605eb1889924d151959f
Gorun <307532500@qq.com>
2023-12-14 21:12:09 +0800

    添加远程端口测试

--- src/route/index.js
+++ src/route/index.js
@@ -1,10 +1,12 @@
 import {createRouter, createWebHashHistory} from "vue-router";
 import HelloWorld from "@/components/HelloWorld.vue";
 import LoginView from "@/views/login/LoginView.vue";
+import RemoteTester from "@/views/remote_test/RemoteTester.vue";
 
 const routes = [
     {path: '/', component: HelloWorld},
-    {path: '/login', component: LoginView}
+    {path: '/login', component: LoginView},
+    {path: '/test', component: RemoteTester}
 ]
 
 const router = createRouter({
---
+++ src/views/remote_test/RemoteTester.vue
@@ -0,0 +1,29 @@
+<script setup>
+import {ref} from "vue";
+import log from "@/utils/debug";
+import request from "@/utils/request";
+
+const value = ref('')
+
+function onClick1() {
+  log('button clicked')
+  request.get('/hello')
+      .then((received) => {
+        value.value = received.data
+      })
+}
+
+</script>
+
+<template>
+  <div>
+    <h1>Remote Tester!</h1>
+    <p>received value = {{ value }}</p>
+
+    <el-button @click="onClick1">Test</el-button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
