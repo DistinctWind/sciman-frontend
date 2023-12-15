eda42cfcdecf2c9fda51ebd6708c72045b968c2a
Gorun <307532500@qq.com>
2023-12-15 12:14:30 +0800

    添加debugInfo

--- src/route/index.js
+++ src/route/index.js
@@ -2,11 +2,13 @@ import {createRouter, createWebHashHistory} from "vue-router";
 import HelloWorld from "@/components/HelloWorld.vue";
 import LoginView from "@/views/login/LoginView.vue";
 import UserInfoView from "@/views/login/UserInfoView.vue";
+import DebugInfo from "@/views/debug/DebugInfo.vue";
 
 const routes = [
     {path: '/', component: HelloWorld},
     {path: '/login', component: LoginView},
-    {path: '/userinfo', component: UserInfoView}
+    {path: '/userinfo', component: UserInfoView},
+    {path: '/debug', component: DebugInfo}
 ]
 
 const router = createRouter({
---
+++ src/views/debug/DebugInfo.vue
@@ -0,0 +1,25 @@
+<script setup>
+import {computed} from "vue";
+import {useStore} from "vuex";
+const store = useStore()
+const username = computed(()=>store.state.username)
+const id = computed(()=>store.state.id)
+const role = computed(()=>store.state.role)
+const debugInfo = [
+  {username, id, role}
+]
+</script>
+
+<template>
+<div>
+  <el-table :data="debugInfo" style="width: 100%">
+    <el-table-column prop="username" label="Username"/>
+    <el-table-column prop="id" label="Id"/>
+    <el-table-column prop="role" label="Role"/>
+  </el-table>
+</div>
+</template>
+
+<style scoped>
+
+</style>
