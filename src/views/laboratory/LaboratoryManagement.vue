ee143f2894df765cd02529d761b54d1354900d94
Gorun <307532500@qq.com>
2023-12-15 22:26:08 +0800

    编写实验室框架

--- src/views/laboratory/LaboratoryManagement.vue
+++ src/views/laboratory/LaboratoryManagement.vue
@@ -1,6 +1,6 @@
 <script setup>
 
-import {onMounted, ref} from "vue";
+import {onMounted, reactive, ref} from "vue";
 import {listAllLaboratories} from "@/api/lab/laboratory";
 import log from "@/utils/debug";
 
@@ -11,6 +11,24 @@ onMounted(async () => {
   log(labData.value)
 })
 
+const modifyLabDialogVisible = ref(false)
+const modifyLabForm = reactive({
+  id: 0,
+  name: '',
+  orientation: ''
+})
+
+const modifyLab = (row) => {
+  log(row)
+  modifyLabDialogVisible.value = true
+  modifyLabForm.id = row.id
+  modifyLabForm.name = row.name
+  modifyLabForm.orientation = row.orientation
+}
+
+const modifyLabSubmit = () => {
+
+}
 </script>
 
 <template>
@@ -21,10 +39,35 @@ onMounted(async () => {
       <el-table-column prop="orientation" label="实验室研究方向"/>
       <el-table-column label="操作">
         <template #default="scope">
-          <el-button size="small">编辑{{scope.row.name}}</el-button>
+          <el-button size="small" @click="modifyLab(scope.row)">编辑{{ scope.row.name }}</el-button>
         </template>
       </el-table-column>
     </el-table>
+
+    <el-dialog v-model="modifyLabDialogVisible">
+      <el-form :model="modifyLabForm" label-width="120px">
+        <el-form-item label="实验室ID">
+          <el-input disabled v-model="modifyLabForm.id"/>
+        </el-form-item>
+        <el-form-item label="实验室名称">
+          <el-input v-model="modifyLabForm.name"/>
+        </el-form-item>
+        <el-form-item label="实验室研究方向">
+          <el-input v-model="modifyLabForm.orientation"
+                    :rows="7"
+                    type="textarea"
+                    placeholder="研究方向介绍"/>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="modifyLabSubmit">
+            提交
+          </el-button>
+          <el-button @click="modifyLabDialogVisible=false">
+            取消
+          </el-button>
+        </el-form-item>
+      </el-form>
+    </el-dialog>
   </div>
 </template>
 
