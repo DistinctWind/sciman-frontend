a6e6ded8fb096ff5126e581819f521862658211d
Gorun <307532500@qq.com>
2023-12-15 22:10:09 +0800

    添加前端查询实验室并展示的操作

--- src/views/laboratory/LaboratoryManagement.vue
+++ src/views/laboratory/LaboratoryManagement.vue
@@ -19,6 +19,11 @@ onMounted(async () => {
       <el-table-column prop="id" label="实验室ID" width="180"/>
       <el-table-column prop="name" label="实验室名称" width="180"/>
       <el-table-column prop="orientation" label="实验室研究方向"/>
+      <el-table-column label="操作">
+        <template #default="scope">
+          <el-button size="small">编辑{{scope.row.name}}</el-button>
+        </template>
+      </el-table-column>
     </el-table>
   </div>
 </template>
