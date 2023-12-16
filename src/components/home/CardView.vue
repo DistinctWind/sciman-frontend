e66cb2e0419c64d64d3da97b6aef1f33f1168fb9
Gorun <307532500@qq.com>
2023-12-16 20:31:02 +0800

    增加Home监控View

---
+++ src/components/home/CardView.vue
@@ -0,0 +1,29 @@
+<script setup>
+const prop = defineProps({
+  title: String,
+  count: Number
+})
+</script>
+
+<template>
+  <el-card class="show-card">
+    <template #header>
+      <span class="title">{{prop.title}}</span>
+    </template>
+    <span class="number">{{prop.count}}</span>
+  </el-card>
+</template>
+
+<style scoped>
+.title {
+  font-size: large;
+}
+.number {
+  font-size: xx-large;
+  font-weight: bold;
+}
+.show-card {
+  width: 250px;
+  margin: 10px;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,25 @@
 <script setup>
+import CardView from "@/components/home/CardView.vue";
+import {reactive} from "vue";
 
+const count = reactive({
+  lab: 0,
+  researcher: 0,
+  secretary: 0,
+})
 </script>
 
 <template>
-<div>
-  <h1>This is home view</h1>
-</div>
+  <div class="dashboard">
+    <CardView title="实验室" :count="count.lab"/>
+    <CardView title="研究人员" :count="count.researcher"/>
+    <CardView title="秘书" :count="count.researcher"/>
+  </div>
 </template>
 
 <style scoped>
-
+.dashboard {
+  display: flex;
+  flex-wrap: wrap;
+}
 </style>
