2c7a27826fec5e16fe1752ccb1f16805e3af58ff
Gorun <307532500@qq.com>
2023-12-25 09:48:54 +0800

    添加分页条

--- src/views/personal_management/ResearcherManage.vue
+++ src/views/personal_management/ResearcherManage.vue
@@ -24,33 +24,34 @@ const query = async () => {
 </script>
 
 <template>
-<div>
-  <el-container>
-    <el-header>
-      <el-row>
+  <div>
+    <el-container>
+      <el-header>
         <el-row>
-          <span style="margin-right: 10px">姓名</span>
-          <el-input class="input" v-model="queryParam.nameFilter" @keyup.enter="query"/>
+          <el-row>
+            <span style="margin-right: 10px">姓名</span>
+            <el-input class="input" v-model="queryParam.nameFilter" @keyup.enter="query"/>
+          </el-row>
+          <el-button class="query" type="primary" @click="query">查询</el-button>
         </el-row>
-        <el-button class="query" type="primary" @click="query">查询</el-button>
-      </el-row>
-    </el-header>
-    <el-main>
-      <el-table :data="researcherData" border style="width: 100%">
-        <el-table-column prop="employeeId" label="工号" width="180"/>
-        <el-table-column prop="laboratoryName" label="所属实验室" width="180"/>
-        <el-table-column prop="name" label="姓名"/>
-        <el-table-column prop="gender" label="性别"/>
-        <el-table-column prop="age" label="年龄"/>
-        <el-table-column label="操作">
-          <template #default="scope">
-            <el-button size="small" @click="scope.row">详情</el-button>
-          </template>
-        </el-table-column>
-      </el-table>
-    </el-main>
-  </el-container>
-</div>
+      </el-header>
+      <el-main class="el-main">
+        <el-table :data="researcherData" border style="width: 100%">
+          <el-table-column prop="employeeId" label="工号" width="180"/>
+          <el-table-column prop="laboratoryName" label="所属实验室" width="180"/>
+          <el-table-column prop="name" label="姓名"/>
+          <el-table-column prop="gender" label="性别"/>
+          <el-table-column prop="age" label="年龄"/>
+          <el-table-column label="操作">
+            <template #default="scope">
+              <el-button size="small" @click="scope.row">详情</el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+        <el-pagination class="pagination" background layout="prev, pager, next" :total="1000"/>
+      </el-main>
+    </el-container>
+  </div>
 </template>
 
 <style scoped>
@@ -67,5 +68,21 @@ const query = async () => {
   display: flex;
   justify-content: space-between;
   align-items: center;
+}
+
+.el-table {
+  margin-bottom: 20px;
+}
+
+.el-pagination {
+  margin-top: 20px;
+}
+
+.el-main {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
 }
 </style>
