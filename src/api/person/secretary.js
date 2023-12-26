1dcb2ba7783433cb8cf6ce3e00646978e74d0d7d
Gorun <307532500@qq.com>
2023-12-26 23:59:01 +0800

    添加相关API调用方法

--- src/api/person/secretary.js
+++ src/api/person/secretary.js
@@ -3,3 +3,11 @@ import request from "@/utils/request";
 export const listSecretary = (queryParam) => {
     return request.post('/person/secretary/list', queryParam)
 }
+
+export const secretaryDetail = (id) => {
+    return request.get(`/person/secretary/detail/${id}`)
+}
+
+export const modifySecretary = (secretary) => {
+    return request.post('/person/secretary/modify', secretary)
+}
