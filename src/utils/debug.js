3bc8473c6fde313730c2012a779a3405bb5a23f4
Gorun <307532500@qq.com>
2023-12-26 21:00:01 +0800

    添加快速debug功能

--- src/utils/debug.js
+++ src/utils/debug.js
@@ -1,3 +1,5 @@
+import {saveUserInfo} from "@/utils/storeage";
+
 export const debug = true
 
 const log = (message) => {
@@ -5,5 +7,17 @@ const log = (message) => {
         console.log(message)
     }
 }
+
+export const loginAsAdmin = () => {
+    log('Login using admin account')
+    const info = {
+        username: 'admin',
+        role: 'admin',
+        id: 2
+    }
+    saveUserInfo(info)
+}
+
+export const debugTarget = '/secretaryManagement'
 
 export default log
