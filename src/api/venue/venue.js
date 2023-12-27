e5b72fb7943f12f92c2f981cc4ee9e0e689ccb7e
Gorun <307532500@qq.com>
2023-12-27 19:10:52 +0800

    添加修改实验室信息的UI界面

--- src/api/venue/venue.js
+++ src/api/venue/venue.js
@@ -7,3 +7,7 @@ export const listVenue = (queryParam) => {
 export const getVenueDetail = (id) => {
     return request.get(`/venue/detail/${id}`)
 }
+
+export const modifyVenue = (venue) => {
+    return request.post('/venue/modify', venue)
+}
