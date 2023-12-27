ba2c5d51dc3208c207752752d867fc1071b3024a
Gorun <307532500@qq.com>
2023-12-27 19:57:23 +0800

    添加实验室展示UI

--- src/views/laboratory/LaboratoryManagement.vue
+++ src/views/laboratory/LaboratoryManagement.vue
@@ -1,77 +1,122 @@
 <script setup>
-
 import {onMounted, reactive, ref} from "vue";
 import log from "@/utils/debug";
-import {listAllLabIntros} from "@/api/lab/laboratory";
+import {listLaboratory} from "@/api/lab/laboratory";
 
-const labData = ref([])
+const tableData = ref([])
+const tableTotal = ref(10)
+const queryParam = reactive({
+  page: 1,
+  pageSize: 10
+})
+
+const query = async () => {
+  log(queryParam)
+  const result = await listLaboratory(queryParam)
+  const response = result.data
+  tableData.value = response.data.laboratory
+  tableTotal.value = response.data.total
+}
+
 onMounted(async () => {
-  const response = await listAllLabIntros()
-  log(response.data)
-  labData.value = response.data.data
+  await query()
 })
-
-const modifyLabDialogVisible = ref(false)
-const labForm = reactive({
-  id: -1,
-  name: '',
-  orientation: ''
-})
-
-const modifyLab = (row) => {
-  log(row)
-  modifyLabDialogVisible.value = true
-  labForm.id = row.id
-  labForm.name = row.laboratoryName
-  // labForm.orientation = row.orientation
-}
-
-const modifyLabSubmit = () => {
-
-}
 </script>
 
 <template>
   <div>
-    <el-table :data="labData" border style="width: 100%">
-      <el-table-column prop="id" label="实验室ID" width="180"/>
-      <el-table-column prop="laboratoryName" label="实验室名称" width="180"/>
-      <el-table-column prop="stuffName" label="主任"/>
-      <el-table-column prop="secretaryName" label="秘书"/>
-      <el-table-column label="操作">
-        <template #default="scope">
-          <el-button size="small" @click="modifyLab(scope.row)">编辑{{ scope.row.name }}</el-button>
-        </template>
-      </el-table-column>
-    </el-table>
-
-    <el-dialog v-model="modifyLabDialogVisible">
-      <el-form :model="labForm" label-width="120px">
-        <el-form-item label="实验室ID">
-          <el-input disabled v-model="labForm.id"/>
+    <el-container>
+      <el-header>
+        <el-row>
+          <el-row>
+            <span style="margin-right: 10px">名称</span>
+            <el-input class="input" v-model="queryParam.laboratoryNameFilter" @keyup.enter="query"/>
+          </el-row>
+          <div class="query">
+            <el-button type="primary" @click="query">查询</el-button>
+            <el-button type="success" @click="insert">新增</el-button>
+          </div>
+        </el-row>
+      </el-header>
+      <el-main class="el-main">
+        <el-table :data="tableData" border style="width: 100%">
+          <el-table-column prop="id" label="实验室ID" width="180"/>
+          <el-table-column prop="laboratoryName" label="实验室名称" width="180"/>
+          <el-table-column prop="stuffName" label="主任"/>
+          <el-table-column prop="secretaryName" label="秘书"/>
+          <el-table-column label="操作">
+            <template #default="scope">
+              <el-button size="small" @click="modifyVenueOf(scope.row)">修改</el-button>
+              <el-button size="small" type="danger" @click="deleteVenueOf(scope.row)">删除</el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+        <el-pagination class="pagination" background
+                       :page-sizes="[5, 10, 20]"
+                       layout="sizes, prev, pager, next, total"
+                       v-model:current-page="queryParam.page"
+                       v-model:total="tableTotal"
+                       v-model:page-size="queryParam.pageSize"
+                       @size-change="query"
+                       @current-change="query"/>
+      </el-main>
+    </el-container>
+    <el-dialog v-model="dataDialogVisible">
+      <el-form :model="dialogData" label-width="120px">
+        <el-form-item label="场地ID" v-if="idVisible">
+          <el-input v-model="dialogData.id" disabled/>
         </el-form-item>
-        <el-form-item label="实验室名称">
-          <el-input v-model="labForm.name"/>
+        <el-form-item label="所属实验室">
+          <LaboratorySelection v-model="dialogData.laboratoryId"/>
         </el-form-item>
-        <el-form-item label="实验室研究方向">
-          <el-input v-model="labForm.orientation"
-                    :rows="7"
-                    type="textarea"
-                    placeholder="研究方向介绍"/>
+        <el-form-item label="面积（㎡）">
+          <el-input v-model="dialogData.area" oninput="value=value.replace(/[^\d.]/g, '')"/>
         </el-form-item>
-        <el-form-item>
-          <el-button type="primary" @click="modifyLabSubmit">
-            提交
-          </el-button>
-          <el-button @click="modifyLabDialogVisible=false">
-            取消
-          </el-button>
+        <el-form-item label="地址">
+          <el-input v-model="dialogData.address"/>
         </el-form-item>
       </el-form>
+      <template #footer>
+      <span class="dialog-footer">
+        <el-button @click="dataDialogVisible = false">取消</el-button>
+        <el-button type="primary" @click="confirm">
+          确认
+        </el-button>
+      </span>
+      </template>
     </el-dialog>
   </div>
 </template>
 
 <style scoped>
+.input {
+  width: 250px;
+  align-self: flex-start;
+}
 
+.query {
+  align-self: flex-end;
+}
+
+.el-header .el-row {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.el-table {
+  margin-bottom: 20px;
+}
+
+.el-pagination {
+  margin-top: 20px;
+}
+
+.el-main {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+}
 </style>
