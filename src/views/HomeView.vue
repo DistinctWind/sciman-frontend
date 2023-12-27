20fb17195e741f4aeca21e9c57a4faecbfc79e13
Gorun <307532500@qq.com>
2023-12-27 19:41:52 +0800

    添加办公场地计数UI

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,14 +8,17 @@ const count = reactive({
   lab: 0,
   researcher: 0,
   secretary: 0,
+  venue: 0
 })
 
 onMounted(async () => {
-  const {data: response} = await getCount()
+  const result = await getCount()
+  const response = result.data
   log(response)
   count.lab = response.data.laboratory
   count.researcher = response.data.researcher
   count.secretary = response.data.secretary
+  count.venue = response.data.venue
 })
 </script>
 
@@ -24,6 +27,7 @@ onMounted(async () => {
     <CardView title="实验室" :count="count.lab"/>
     <CardView title="研究人员" :count="count.researcher"/>
     <CardView title="秘书" :count="count.secretary"/>
+    <CardView title="办公场地" :count="count.venue"/>
   </div>
 </template>
 
