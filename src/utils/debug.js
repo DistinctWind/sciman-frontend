b0631bf668be4d4f704c45edc4a0423e5fe9a93a
Gorun <307532500@qq.com>
2023-12-27 18:35:50 +0800

    调整Venue展示信息

--- src/utils/debug.js
+++ src/utils/debug.js
@@ -18,6 +18,6 @@ export const loginAsAdmin = () => {
     saveUserInfo(info)
 }
 
-export const debugTarget = '/secretaryManagement'
+export const debugTarget = '/venueManagement'
 
 export default log
