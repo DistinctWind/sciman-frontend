9df44e1b652bb6860cce6b1ea01cb2528b2d0e33
Gorun <307532500@qq.com>
2024-01-03 09:06:00 +0800

    添加展示更多计数信息的UI

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,29 +5,38 @@ import {getCount} from "@/api/count/count";
 import log from "@/utils/debug";
 
 const count = reactive({
-  lab: 0,
+  laboratory: 0,
   researcher: 0,
   secretary: 0,
-  venue: 0
+  venue: 0,
+  organization: 0,
+  project: 0,
+  contact: 0,
+  achievement: 0,
 })
 
 onMounted(async () => {
   const result = await getCount()
-  const response = result.data
+  const response = result.data.data
   log(response)
-  count.lab = response.data.laboratory
-  count.researcher = response.data.researcher
-  count.secretary = response.data.secretary
-  count.venue = response.data.venue
+
+  const keys = Object.keys(count)
+  keys.forEach(key => {
+    count[key] = response[key]
+  })
 })
 </script>
 
 <template>
   <div class="dashboard">
-    <CardView title="实验室" :count="count.lab"/>
+    <CardView title="实验室" :count="count.laboratory"/>
     <CardView title="研究人员" :count="count.researcher"/>
     <CardView title="秘书" :count="count.secretary"/>
     <CardView title="办公场地" :count="count.venue"/>
+    <CardView title="外部单位" :count="count.organization"/>
+    <CardView title="项目" :count="count.project"/>
+    <CardView title="联系人" :count="count.contact"/>
+    <CardView title="科研成果" :count="count.achievement"/>
   </div>
 </template>
 
