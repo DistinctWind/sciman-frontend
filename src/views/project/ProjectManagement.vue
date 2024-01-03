a9700a71af71d0af6de7ab6d0af7ad8f869ac11f
Gorun <307532500@qq.com>
2024-01-03 12:43:53 +0800

    修复出现bug的功能

--- src/views/project/ProjectManagement.vue
+++ src/views/project/ProjectManagement.vue
@@ -3,6 +3,7 @@ import {onMounted, reactive, ref} from "vue";
 import {listProject} from "@/api/project/project";
 import log from "@/utils/debug";
 import {useRouter} from "vue-router";
+import {notImplemented} from "@/utils/implementattion";
 
 const tableData = ref([])
 const tableTotal = ref(0)
@@ -60,7 +61,7 @@ const showDetailOf = (project) => {
           <el-table-column label="操作">
             <template #default="scope">
               <el-button size="small" @click="showDetailOf(scope.row)">详情</el-button>
-              <el-button size="small" type="danger" @click="deleteSecretaryOf(scope.row)">删除</el-button>
+              <el-button size="small" type="danger" @click="notImplemented">删除</el-button>
             </template>
           </el-table-column>
         </el-table>
